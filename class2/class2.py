2b133ff26570039969ad5ef9f94245be74a8e44f
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-11-04 21:22:39 +0500

    different type of strings and f-strings jinja

---
+++ class2/class2.py
@@ -0,0 +1,12 @@
+z=2
+print(z)
+#variable 
+firstName  : str = "zarnain"
+F_Name :  str = "Abid"
+print(firstName);
+print(F_Name);
+name : str = "alina"
+print(type(name))
+# convert any special char into single char by using / before char 
+studentName : str  ='i am student of PIAIC \n  and my father\'s name is " abid"'
+print(studentName)
